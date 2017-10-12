87a65b0b590f84b541229cf54d641f7f5a06478b
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2017-10-12 19:51:32 +0100

    Testing a command turns into an event

--- EventCheckout/EventCheckout/ReadAndWriteModels.cs
+++ EventCheckout/EventCheckout/ReadAndWriteModels.cs
@@ -12,10 +12,54 @@ namespace EventCheckout.Tests
         // a command can be written to the event stream
         public void ACommandBecomesAnEvent()
         {
+            var eventStream = new EventStream();
+            var basket = new Basket(eventStream);
+            basket.ScanItem("Screwdriver");
+            Assert.Equal(1, eventStream.Events.Count);
+            Assert.True(eventStream.Events.First() is ItemAdded);
         }
     }
- 
-   public class ReadModelTests
+
+    public class ItemAdded{
+        public ItemAdded(string sku)
+        {
+            Sku = sku;
+        }
+
+        public string Sku { get; }
+    }
+
+    internal class Basket
+    {
+        private EventStream eventStream;
+
+        public Basket(EventStream eventStream)
+        {
+            this.eventStream = eventStream;
+        }
+
+        internal void ScanItem(string sku)
+        {
+            var itemAdded = new ItemAdded(sku);
+            eventStream.Add(itemAdded);
+        }
+    }
+
+    internal class EventStream
+    {
+        public EventStream()
+        {
+        }
+
+        public List<ItemAdded> Events { get; internal set; } = new List<ItemAdded>();
+
+        internal void Add(ItemAdded evt)
+        {
+            Events.Add(evt);
+        }
+    }
+
+    public class ReadModelTests
     {
         [Fact]
         // the event stream can be used to generate a running total
